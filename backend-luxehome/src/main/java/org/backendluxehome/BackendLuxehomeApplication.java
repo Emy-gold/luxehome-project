f4c9afd443addd102c61c517d7c56b30330e1eb2
Emy-gold <imanelgarate@gmail.com>
2026-03-15 22:23:31 +0000

    create the backend project with spring boot

---
+++ backend-luxehome/src/main/java/org/backendluxehome/BackendLuxehomeApplication.java
@@ -0,0 +1,13 @@
+package org.backendluxehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLuxehomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendLuxehomeApplication.class, args);
+    }
+
+}
---
+++ backend-luxehome/src/test/java/org/backendluxehome/BackendLuxehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backendluxehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLuxehomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
