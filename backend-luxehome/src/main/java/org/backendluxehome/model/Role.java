a77db1e4cae78386083c6fce83d0a5849732dfae
Emy-gold <imanelgarate@gmail.com>
2026-03-26 21:42:38 +0100

    finish the user and the role entities

---
+++ backend-luxehome/src/main/java/org/backendluxehome/model/Role.java
@@ -0,0 +1,40 @@
+package org.backendluxehome.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="roles")
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id_role;
+    @Column(unique = true)
+    private String roleName;
+
+    //create a relationship between users and roles
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private Date createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private Date lastModifiedDate;
+}
