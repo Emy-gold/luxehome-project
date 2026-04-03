c6f0f00662d818235864673c53d0d547f8d77c81
Emy-gold <imanelgarate@gmail.com>
2026-04-03 22:34:06 +0100

    add the token entity and the token repo

---
+++ backend-luxehome/src/main/java/org/backendluxehome/model/Token.java
@@ -0,0 +1,30 @@
+package org.backendluxehome.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String token;
+    private LocalDate createdAt;
+    private LocalDate expiredAt;
+    private LocalDate validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId",nullable = false)
+    private User user;
+}
