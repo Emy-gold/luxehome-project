961d514aef67c066e195e74d3a6918e15e456535
Emy-gold <imanelgarate@gmail.com>
2026-04-07 22:08:12 +0100

    add validation to the dto

--- backend-luxehome/src/main/java/org/backendluxehome/dto/RegistrationRequest.java
+++ backend-luxehome/src/main/java/org/backendluxehome/dto/RegistrationRequest.java
@@ -1,5 +1,7 @@
 package org.backendluxehome.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +13,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RegistrationRequest {
 
+    @NotEmpty(message = "Firstname is required")
+    @NotBlank(message = "Firstname is required")
     private String firstname;
+    @NotEmpty(message = "Lastname is required")
+    @NotBlank(message = "Lastname is required")
     private String lastname;
+    @NotEmpty(message = "Email is required")
+    @NotBlank(message = "Email is required")
     private String email;
+    @NotEmpty(message = "Password is required")
+    @NotBlank(message = "Password is required")
     private String password;
 }
