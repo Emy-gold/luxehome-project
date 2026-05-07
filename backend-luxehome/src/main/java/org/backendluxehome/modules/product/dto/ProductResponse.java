9c4a5f465544b983c3215350acb6a52fc70143ec
Emy-gold <imanelgarate@gmail.com>
2026-05-07 11:44:29 +0100

    add the response class

---
+++ backend-luxehome/src/main/java/org/backendluxehome/modules/product/dto/ProductResponse.java
@@ -0,0 +1,22 @@
+package org.backendluxehome.modules.product.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int stock;
+    private String categoryName;
+    private String imageUrl;
+
+}
